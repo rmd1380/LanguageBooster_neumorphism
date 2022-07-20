c2658db614bfecdeb73e762071671ae58dc4750f
Rasoul <RasoulMotie1380@gmail.com>
2022-07-20 11:53:45 +0430

    AddWordActivity created.

---
+++ app/src/main/java/com/example/languagebooster/AddWordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.languagebooster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddWordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_word)
+    }
+}
